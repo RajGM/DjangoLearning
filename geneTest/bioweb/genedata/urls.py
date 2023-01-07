ae459efe0017d4372e1f8f62c0ac6ba103e76869
Raj Gaurav Maurya <rajgmsocial19@gmail.com>
2023-01-08 03:02:23 +0530

    Feat:Gene add working with validators

--- geneTest/bioweb/genedata/urls.py
+++ geneTest/bioweb/genedata/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('list/<str:type>', views.list, name='list'),
     path('poslist/', views.poslist, name='poslist'),
     path('delete/<int:pk>', views.delete, name='delete'),
+    path('create_ec/', views.create_ec, name='create'),
+    path('create_gene/', views.create_gene, name='create_gene'),
 ]
--- geneTest/bioweb/genedata/views.py
+++ geneTest/bioweb/genedata/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from .models import *
 from django.http import HttpResponseRedirect
+from .models import *
+from .forms import *
 
 # Create your views here.
 
 def index(request):
     master_genes = Gene.objects.all()
     return render(request, 'genedata/index.html', {'master_genes': master_genes})
-
 
 def gene(request, pk):
     gene = Gene.objects.get(pk=pk)
@@ -17,12 +17,10 @@ def gene(request, pk):
     master_genes = Gene.objects.all()
     return render(request, 'genedata/gene.html', {'gene': gene, 'master_genes': master_genes})
 
-
 def list(request, type):
     genes = Gene.objects.filter(entity__exact=type)
     master_genes = Gene.objects.all()
     return render(request, 'genedata/list.html', {'genes': genes, 'type': type,'master_genes': master_genes})
-
 
 def poslist(request):
     genes = Gene.objects.filter(
@@ -31,8 +29,38 @@ def poslist(request):
     return render(request, 'genedata/list.html', {'genes': genes, 'type':
                                                   'PosList','master_genes': master_genes})
 
-
 def delete(request, pk):
     GeneAttributeLink.objects.filter(gene_id=pk).delete()
     Gene.objects.filter(pk=pk).delete()
     return HttpResponseRedirect("/")
+
+def create_ec(request):
+    master_genes = Gene.objects.all()
+    if request.method == 'POST':
+        form = ECForm(request.POST)
+        if form.is_valid():
+            ec = EC()
+            ec.ec_name = form.cleaned_data['ec_name']
+            ec.save()
+            return HttpResponseRedirect('/create_ec/')
+    else:
+        ecs = EC.objects.all()
+        form = ECForm()
+    return render(request, 'genedata/ec.html', {'form': form,'ecs': ecs, 'master_genes': master_genes})    
+
+def create_gene(request):
+    master_genes = Gene.objects.all()
+    if request.method == 'POST':
+        form = GeneForm(request.POST)
+        if form.is_valid():
+            gene = form.save()
+            return HttpResponseRedirect('/create_ec/')
+        else:
+            return render(request, 'genedata/create_gene.html', {'error':"failed", 'master_genes': master_genes, 'form': form})
+    else:
+        form = GeneForm()
+        master_genes = Gene.objects.all()
+    return render(request, 'genedata/create_gene.html', {'form':form, 'master_genes': master_genes})
+
+
+    
