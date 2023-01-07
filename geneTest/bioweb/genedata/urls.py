452a8b21c57b64ac894a6160320d8a6488b87895
Raj Gaurav Maurya <rajgmsocial19@gmail.com>
2023-01-08 00:22:49 +0530

    Delete function added

---
+++ geneTest/bioweb/genedata/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('test', views.index, name='index'),
+    path('gene/<int:pk>',views.gene,name='gene'),
+    path('list/<str:type>', views.list, name='list'),
+    path('poslist/', views.poslist, name='poslist'),
+    path('delete/<int:pk>', views.delete, name='delete'),
+]
